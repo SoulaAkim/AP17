ebeb509be1efbf71541592a338ee7968c6ae1924
soula <soula.akim@gmail.com>
2018-03-12 16:35:57 +0100

    feat(registration):Add registration form

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,1 +1,35 @@
 <h1>inscription</h1>
+<?php
+
+if(isset($_POST['frmRegistration'])) {
+    $name = $_POST['name'] ?? "";
+    $firstName = $_POST['firstName'] ?? "";
+    $mail = $_POST['mail'] ?? "";
+    $password = $_POST['password'] ?? "";
+
+
+    $erreurs = array();
+
+    if($name == "") array_push($erreurs, "Vuillez saisir un nom");
+    if($firstName == "") array_push($erreurs, "Vuillez saisir un prenom");
+    if($mail == "") array_push($erreurs, "Vuillez saisir un mail");
+    if($password == "") array_push($erreurs, "Vuillez saisir un mot de passe");
+
+  if(count($erreurs) > 0 ) {
+      $message = "<ul>";
+      for ($i =0; $i < count($erreurs); $i++) {
+          $message .= "<li>";
+          $message .= $erreurs[$i];
+          $message .= "</li>";
+      }
+      $message .= "</ul>";
+      echo $message;
+      include "frmregistration.php";
+  }
+
+
+} else {
+
+    include "frmregistration.php";
+}
+
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <title>CESI AP - Blog/e-commerce</title>
+    <link rel="stylesheet"  href="./assets/css/style.css">
+</head>
+
 <?php
 include('./includes/header.php');
 include('./includes/footer.php');
@@ -9,10 +16,9 @@ $files = glob("./includes/*.inc.php");
 if (in_array($page, $files)) {
     include $page;
 } else {
-    include "./includes/homes.inc.php";
+    include "./includes/home.php";
 }
 
 
 
-include "./includes/$page.php";
 
