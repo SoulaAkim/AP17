c3f77acc7b3d91a532300939da8ab6dc94aa6d7d
soula <soula.akim@gmail.com>
2018-03-16 14:49:16 +0100

    feat(articles):Post articles

--- includes/frmregistration.php
+++ includes/frmregistration.php
@@ -1,24 +1,34 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+
+
+
 <div class="form-group">
 <form action="#" method="post">
     <div>
         <label for="name">Nom : </label>
-        <input type="text" id="name" name="name" value ="<?php if(isset($name)) echo $name ; ?>" />
+        <input  class="form-control" type="text" id="name" name="name" value ="<?php if(isset($name)) echo $name ; ?>" />
     </div>
     <div>
         <label for="firstName">Prénom : </label>
-        <input type="text" id="firstName" name="firstName" value ="<?php if(isset($firstName)) echo $firstName ; ?>"/>
+        <input class="form-control" type="text" id="firstName" name="firstName" value ="<?php if(isset($firstName)) echo $firstName ; ?>"/>
     </div>
     <div>
         <label for="mail">e-mail : </label>
-        <input type="text" id="mail" name="mail" value ="<?php if(isset($mail)) echo $mail ; ?>" />
+        <input  class="form-control" type="email" id="mail" name="mail" value ="<?php if(isset($mail)) echo $mail ; ?>" />
     </div>
     <div>
         <label for="password">Mot de passe : </label>
-        <input type="password" id="password" name="password" />
+        <input class="form-control" type="password" id="password" name="password" />
     </div>
     <div>
-        <input type="reset" value="Effacer" />
-        <input type="submit" value="Envoyer" name="frmRegistration" />
+        <input class="btn btn-secondary" type="reset" value="Effacer" />
+        <input class="btn btn-primary" type="submit" value="Envoyer" name="frmRegistration" />
     </div>
 </form>
+</div>
+</div>
+</div>
+
 </div>
